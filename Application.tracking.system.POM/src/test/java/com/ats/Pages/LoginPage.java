e0345d10062c81ee79c4ff00335c9486a0ef23a9
Sunil More <61233852+SunilMore007@users.noreply.github.com>
2021-04-07 12:05:19 +0530

    Application.tracking.system.pom project

---
+++ Application.tracking.system.POM/src/test/java/com/ats/Pages/LoginPage.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.ats.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author ADMIN this class will store all the locators and methods of login
+ *         page
+ */
+public class LoginPage {
+
+	WebDriver driver;
+
+	By Username = By.id("email");
+	By Password = By.id("pass");
+	By loginbutton = By.name("login");
+
+	public LoginPage(WebDriver driver) {
+
+		this.driver = driver;
+	}
+
+	public void typeusername() {
+
+		driver.findElement(Username).sendKeys("manjula@talentrecruit.in");
+
+	}
+
+	public void typepassword() {
+
+		driver.findElement(Password).sendKeys("dummytalent");
+
+	}
+
+	public void loginbutton() {
+		driver.findElement(loginbutton).click();
+
+	}
+
+}
---
+++ Application.tracking.system.POM/src/test/java/com/ats/testcases/Verifyatslogin.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.ats.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.ats.Pages.LoginPage;
+
+/**
+ * @author ADMIN
+ *
+ */
+public class Verifyatslogin {
+
+	@Test
+	public void Verifyvalidlogin() {
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get(
+				"https://www.facebook.com/?stype=lo&jlou=AfeeqhsdMThSEj7oNY1ptQ4Qt4PExH6-G_j-TXrOxt5pcUXpPsnXhX_OFG3VPNir2j0-cGpJjwZynQhmWeEQ7Y939trb9h2CLMW5Qp60s_HUaw&smuh=62133&lh=Ac9L6OY0LSx9XHRRNfI");
+
+		LoginPage login = new LoginPage(driver);
+		login.typeusername();
+		login.typepassword();
+		login.loginbutton();
+
+		driver.quit();
+
+	}
+
+}
